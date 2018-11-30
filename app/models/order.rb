38a5a491fca5dc1f9b73a2ec5517112f9e616673
Wouter Verhaeghe <wouter_verhaeghe@hotmail.com>
2018-11-30 13:00:55 +0100

    added validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :customer
   belongs_to :product
+  validates :size, uniqueness: true, presence: true
 end
--- app/models/seller.rb
+++ app/models/seller.rb
@@ -1,2 +1,5 @@
 class Seller < ApplicationRecord
+  has_many :products
+  validates :contact_person, presence: true
+  validats :address, uniqueness: true, presence: true
 end

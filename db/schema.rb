773b367e9bca028f1b82bac6b647012e6609a325
Wouter Verhaeghe <wouter_verhaeghe@hotmail.com>
2019-01-03 16:47:06 +0100

    removed customers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_09_121217) do
-
-  create_table "customers", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "city"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2019_01_03_154454) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "size"
